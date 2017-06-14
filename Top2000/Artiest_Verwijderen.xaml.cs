3a1c59d9acc8921b41aef765ace0b2fefa5cedfc
MennodeLange <mennodelange@live.nl>
2017-06-14 15:04:43 +0200

    Fix connection string

--- Top2000/Artiest_Verwijderen.xaml.cs
+++ Top2000/Artiest_Verwijderen.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Configuration;
-
 using System.Data.SqlClient;
 using System.Data.Sql;
 using System.Data;
